c1f8991c7571d69f6bed9c8f0647d5feb0d54c9b
liuwq3 <liuwq3@asiainfo.com>
2018-04-23 13:38:05 +0800

    add datasourceSeparator  and needSeparator

--- src/main/java/com/asiainfo/ocdp/flume/adapter/interceptor/Constants.java
+++ src/main/java/com/asiainfo/ocdp/flume/adapter/interceptor/Constants.java
@@ -3,19 +3,21 @@ package com.asiainfo.ocdp.flume.adapter.interceptor;
 
 public class Constants {
 	public static final String KEY = "key";
-	public static final String SEPARATOR = "separator";
-	public static final String ROWNUMBER = "rowNumber";
+	public static final String DATASOURCE_SEPARATOR = "datasourceSeparator";
+	public static final String NEED_SEPARATOR = "needSeparator";
+	public static final String RIELDNUMBER = "fieldNumber";
 	public static final String KEYLOCATION = "keyLocation";
 	public static final String TIMELOCATION = "timeLocation";
 	public static final String DATAFORMAT = "dateFormat";
 	public static final String DATASOURCE_TYPE = "dataSourceType";
-	
+
 	public static final String DATASOURCE_DEFAULT_TYPE = "23G";
+	public static final String DATASOURCE_SEPARATOR_DEFAULT = "|";
+	public static final String NEED_SEPARATOR_DEFAULT = "|";
 	
-	public static final int ROWNUMBER_DEFAULT = 0;
+	public static final int FIELDNUMBER_DEFAULT = 0;
 	public static final int KEYLOCATION_DEFAULT = 0;
-	public static final int  TIMELOCATION_DEFAULT = 0;
+	public static final int TIMELOCATION_DEFAULT = 0;
 	
-	public static final String SEPARATOR_SYMBOL = "|";
 	public static final String DATETIME_WITH_HYPHEN_COLON_MS = "yyyy-MM-dd HH:mm:ss.SSS";
 }
--- src/main/java/com/asiainfo/ocdp/flume/adapter/interceptor/TransformFieldsInterceptor.java
+++ src/main/java/com/asiainfo/ocdp/flume/adapter/interceptor/TransformFieldsInterceptor.java
@@ -31,8 +31,10 @@ a1.sources.s1.kafka.consumer.group.id = test
 
 a1.sources.s1.interceptors = i2
 a1.sources.s1.interceptors.i2.type = com.asiainfo.ocdp.flume.adapter.interceptor.TransformFieldsInterceptor$Builder
-a1.sources.s1.interceptors.i2.separator = \|
-a1.sources.s1.interceptors.i2.rowNumber = 10
+a1.sources.s1.interceptors.i2.datasourceSeparator = \|
+a1.sources.s1.interceptors.i2.needSeparator = \|
+
+a1.sources.s1.interceptors.i2.fieldNumber = 10
 a1.sources.s1.interceptors.i2.keyLocation = 1
 a1.sources.s1.interceptors.i2.timeLocation = 4
 a1.sources.s1.interceptors.i2.dateFormat  = yyyy-MM-dd HH:mm:ss.SSS
@@ -43,55 +45,59 @@ a1.channels.c1.type = org.apache.flume.channel.kafka.KafkaChannel
 a1.channels.c1.kafka.bootstrap.servers = host-10-1-241-58:6667
 a1.channels.c1.kafka.topic = ocspOutput
 a1.channels.c1.parseAsFlumeEvent = false
+        并发度      执行时间     一个批次
  *
  */
 public class TransformFieldsInterceptor implements Interceptor{
 
 	private static final Logger logger = LoggerFactory.getLogger(TransformFieldsInterceptor.class);
-	// 分隔符
-	private String separator = "";
+	// 数据源分隔符
+	private String datasourceSeparator = "";
+	// 需要的分隔符
+	private String needSeparator = "";
 	// 日期格式
 	private String dateFormat = "";
 	// 数据来源
 	private String dataSource = "";
 	// 列数
-	private int rowNumber;
+	private int fieldNumber;
 	// key的位置
 	private int keyLocation;
 	// 时间字段位置
 	private int timeLocation;
 	
 
-	private TransformFieldsInterceptor(String separator, String dateFormat, String dataSource, int rowNumber,
+	private TransformFieldsInterceptor(String datasourceSeparator,String needSeparator, String dateFormat, String dataSource, int fieldNumber,
 			int keyLocation, int timeLocation) {
-		this.separator = separator;
+		this.datasourceSeparator = datasourceSeparator;
+		this.needSeparator = datasourceSeparator;
 		this.dateFormat = dateFormat;
 		this.dataSource = dataSource;
-		this.rowNumber = rowNumber;
+		this.fieldNumber = fieldNumber;
 		this.keyLocation = keyLocation;
 		this.timeLocation = timeLocation;
 	}
     @Override
 	public Event intercept(Event event) {
-		try {
+		try {	
 			Map<String, String> headers = event.getHeaders();
 			String body = new String(event.getBody(), Charsets.UTF_8);
 			if (StringUtils.isNotBlank(body)) {
-				final List<String> valueList = Lists.newArrayList(Splitter.on(separator).trimResults().split(body));
+				final List<String> valueList = Lists.newArrayList(Splitter.on(datasourceSeparator).trimResults().split(body));
 				// 23G
 				if (dataSource.equalsIgnoreCase(Constants.DATASOURCE_DEFAULT_TYPE)) {
 					if (keyLocation < 1 || timeLocation < 1 || keyLocation > valueList.size()
-							|| timeLocation > valueList.size() || rowNumber != valueList.size()) {
+							|| timeLocation > valueList.size() || fieldNumber != valueList.size()) {
 						logger.warn("event  body is  " + body);
 						logger.warn("flume conf keyLocation is " + keyLocation + ", timeLocation is " + timeLocation
-								+ ", rowNumber is " + rowNumber);
+								+ ", rowNumber is " + fieldNumber);
 						throw new IllegalArgumentException("keyLocation or timeLocation or rowNumber  config error !");
 					}
 				} else {
 					if (keyLocation < 1 || keyLocation > valueList.size() || timeLocation > valueList.size()
-							|| rowNumber != valueList.size()) {
+							|| fieldNumber != valueList.size()) {
 						logger.warn("event  body is  " + body);
-						logger.warn("flume conf keyLocation is " + keyLocation + ", rowNumber is " + rowNumber);
+						logger.warn("flume conf keyLocation is " + keyLocation + ", rowNumber is " + fieldNumber);
 						throw new IllegalArgumentException("keyLocation  or rowNumber  config error !");
 					}
 				}
@@ -99,7 +105,7 @@ public class TransformFieldsInterceptor implements Interceptor{
 				int keyIndex = keyLocation - 1;
 				int timeIndex = timeLocation - 1;
 				String keyValue = valueList.get(keyIndex);
-				if (valueList.size() == rowNumber && StringUtils.isNotBlank(keyValue)) {
+				if (StringUtils.isNotBlank(keyValue)) {
 					headers.put(Constants.KEY, keyValue);
 					event.setHeaders(headers);
 					if (dataSource.equalsIgnoreCase(Constants.DATASOURCE_DEFAULT_TYPE)) {
@@ -118,12 +124,12 @@ public class TransformFieldsInterceptor implements Interceptor{
 
 						if (timestamp != 0) {
 							valueList.set(timeIndex, Long.toString(timestamp));
-							String bodyString = Joiner.on(separator).join(valueList);
+							String bodyString = Joiner.on(needSeparator).join(valueList);
 							event.setBody(bodyString.getBytes(Charsets.UTF_8));
 							return event;
 						}
 					} else {
-						String bodyString = Joiner.on(separator).join(valueList);
+						String bodyString = Joiner.on(needSeparator).join(valueList);
 						event.setBody(bodyString.getBytes(Charsets.UTF_8));
 						return event;
 					}
@@ -150,31 +156,35 @@ public class TransformFieldsInterceptor implements Interceptor{
 	}
 
 
-    public static class Builder implements Interceptor.Builder{
-		// 分隔符
-		private String separator = "";
+	public static class Builder implements Interceptor.Builder {
+		// 数据源分隔符
+		private String datasourceSeparator = "";
+		// 需要的分隔符
+		private String needSeparator = "";
 		// 日期格式
 		private String dateFormat = "";
 		// 数据来源
 		private String dataSource = "";
 		// 列数
-		private int rowNumber;
+		private int fieldNumber;
 		// key的位置
 		private int keyLocation;
 		// time的位置
 		private int timeLocation;
         @Override
 		public Interceptor build() {
-			return new TransformFieldsInterceptor(separator, dateFormat, dataSource, rowNumber, keyLocation,
+			return new TransformFieldsInterceptor(datasourceSeparator,needSeparator, dateFormat, dataSource, fieldNumber, keyLocation,
 					timeLocation);
 		}
 
         @Override
 		public void configure(Context context) {
-			this.separator = context.getString(Constants.SEPARATOR, Constants.SEPARATOR_SYMBOL).trim();
+			this.datasourceSeparator = context
+					.getString(Constants.DATASOURCE_SEPARATOR, Constants.DATASOURCE_SEPARATOR_DEFAULT).trim();
+			this.needSeparator = context.getString(Constants.NEED_SEPARATOR, Constants.NEED_SEPARATOR_DEFAULT).trim();
 			this.dateFormat = context.getString(Constants.DATAFORMAT, Constants.DATETIME_WITH_HYPHEN_COLON_MS).trim();
 			this.dataSource = context.getString(Constants.DATASOURCE_TYPE, Constants.DATASOURCE_DEFAULT_TYPE).trim();
-			this.rowNumber = context.getInteger(Constants.ROWNUMBER, Integer.valueOf(Constants.ROWNUMBER_DEFAULT))
+			this.fieldNumber = context.getInteger(Constants.RIELDNUMBER, Integer.valueOf(Constants.FIELDNUMBER_DEFAULT))
 					.intValue();
 			this.keyLocation = context.getInteger(Constants.KEYLOCATION, Integer.valueOf(Constants.KEYLOCATION_DEFAULT))
 					.intValue();
